637f701a44accd27f0dcc5ff0a481dc905955224
Mike Hunsicker <hunsickm@matc.edu>
2023-10-03 14:16:11 -0500

    Completed Activity 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,15 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'client';
-  weatherForecasts: any;
+  posts: any;
 
   constructor(private http: HttpClient) {
 
   }
 
   ngOnInit(): void {
-    this.http.get('http://localhost:5155/weatherforecast').subscribe(
-      response => { this.weatherForecasts = response; },
+    this.http.get('http://localhost:5155/api/posts').subscribe(
+      response => { this.posts = response; },
       error => { console.log(error); }
     );
   }
